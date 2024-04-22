a9cc2395c25b2794416e3b7901793fc7ee3b720a
vasylyeev <vasylyeev@gmail.com>
2024-04-22 15:52:21 +0300

    Change my_app folder place

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:random_background_app/core/injection_container.dart' as di;
 import 'package:random_background_app/core/utils/app_bloc_observer.dart';
-import 'package:random_background_app/my_app/view/my_app.dart';
+import 'package:random_background_app/features/my_app/view/my_app.dart';
 
 void main() async {
   await runZonedGuarded(() async {
